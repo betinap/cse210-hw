ed0ee8818a93eb1dad3e3fc9c851eb6f523a07a9
betinap <betina.paredes@gmail.com>
2024-04-27 10:29:39 -0300

    Add if statement to guess the magic number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.ObjectModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?: ");
+        string answer = Console.ReadLine();
+        int magicNumber = int.Parse(answer);
+
+        Console.Write("What is the guess number?: ");
+        string answer1 = Console.ReadLine();
+        int guessNumber = int.Parse(answer1);
+
+        
+        if (magicNumber > guessNumber)
+        {
+            Console.Write("Higher");
+        }
+        else if (magicNumber < guessNumber)
+        {
+            Console.Write("Lower");
+        }
+        else
+        {
+            Console.Write("You guessed it!");
+        }
     }
 }
