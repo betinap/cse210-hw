f31cd9ed3447111469f5dcf157163df059c1dcdd
betinap <betina.paredes@gmail.com>
2024-04-26 23:11:51 -0300

    Add conditional to determine the letter grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write ("What is your grade percentage? ");
+        string userPercentage = Console.ReadLine();
+        int userGrade = int.Parse(userPercentage);
+
+        string letter = " ";
+
+        if (userGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (userGrade>= 80)
+        {
+            letter = "B";
+        }
+        else if (userGrade>= 70)
+        {
+            letter = "C";
+        }
+        else if (userGrade>= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine ($"Your letter grade is: {letter}");
+
+        if (userGrade >= 70)
+        {
+            Console.WriteLine ("Congratulation, You passed this class!.");
+        }
+        else
+        {
+            Console.WriteLine ("Learn from this experience and come back stronger next time!.");
+        }
+    } 
 }
