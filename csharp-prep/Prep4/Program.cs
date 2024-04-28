93f0d8c94aaa9a9d50a969aefd5ddbbe2510b598
betinap <betina.paredes@gmail.com>
2024-04-27 23:50:01 -0300

    Do a number list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int> ();        
+        int number;
+        
+        do
+        {
+            Console.WriteLine("Enter a number (0 to quit): ");
+            string userResponse = Console.ReadLine();
+            number = int.Parse(userResponse);
+       
+            if (number != 0)
+            {               
+                numbers.Add(number);
+            }
+        } while (number !=0);  
+
+        Console.WriteLine("Enter a number (0 to quit)");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine($"Enter number: {num}");
+        }
+
     }
 }
