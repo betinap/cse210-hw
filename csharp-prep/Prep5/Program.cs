7402894c6af0a04df28567e40b4fb605353f7c07
betinap <betina.paredes@gmail.com>
2024-04-28 01:45:47 -0300

    Diplay welcome message

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        {
+            DisplayWelcome();
+        }
+   
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
     }
+
 }
