52fc4fed3431ad19d17b41d6f9a917bd355fdaa3
betinap <betina.paredes@gmail.com>
2024-05-02 15:17:25 -0300

    Practice the principle of abstraction by creating classes.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Create a job instance.
+        Job job1 = new Job();
+        //Set the member variables and method.
+        job1._jobTitle = "Food Production Manager";
+        job1._company = "(Frigorifico Recreo-Santa Fe)";
+        job1._startYear = 2004;
+        job1._endYear = 2007;
+
+        //Create a new job instance.
+        Job job2 = new Job();
+        //Set variables and method.
+        job2._jobTitle = "Quality Analist";
+        job2._company ="(La Serenisima)";
+        job2._startYear = 2000;
+        job2._endYear = 2004;
+        
+        //Create a new instance of the Resume class.
+        Resume resume = new Resume();
+
+        resume._name = "Betina Paredes";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+
+
+
+
+
     }
 }
