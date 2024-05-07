9e6f5987cef557393e9fa2000f84557a2e326c8c
betinap <betina.paredes@gmail.com>
2024-05-06 23:01:45 -0300

    Change private to public variable

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -2,7 +2,7 @@ using System;
 
 public class PromptGenerator
 {
-    private List<string> _prompts= new List<string> {
+    public List<string> _prompts= new List<string> {
         "Who was the most interesting person I interacted with today?",
         "What was the best part of my day?",
         "How did I see the hand of the Lord in my life today?",
